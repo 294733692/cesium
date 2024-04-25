2c55c00e849df255def609dcc9f245740f1b3bc8
yuyunchao <yuyunchao@jouav.com>
2024-04-25 22:56:25 +0800

    feat: cesium搭建

---
+++ src/views/base/viewer.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="cesiumContainer"></div>
+</template>
+
+<script setup lang="ts">
+import { Viewer } from 'cesium';
+import { onMounted } from 'vue';
+
+
+const initViewer = () => {
+    const cesiumViewer = new Viewer("cesiumContainer")
+}
+
+onMounted(() => {
+    initViewer()
+})
+</script>
+
+<style scoped lang="scss">
+#cesiumContainer {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+}
+</style>
